3f1fbee817819bbad5dab113c60bbf93d3ae8f01
Wasim <wasimxaman13@gmail.com>
2024-03-22 11:43:48 +0500

    Add error handling middleware to the app.js file and update the providerregister.js and admin.js files

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.use(express.json());
 require("./database/db");
 var admin = require("./router/admin");
 app.use(admin);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = null;
+  const success = false;
+  res
+    .status(status)
+    .json({ message: message, status: status, success: success, data: data });
+});
 var swaggerUi = require("swagger-ui-express"),
   swaggerDocument = require("./swagger.json");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
